22be96487d92c02960ce00521ab44686ca005db7
wangjibin <wangjibin@163.com>
2021-06-06 22:01:59 +0800

    提交

---
+++ javascript/src/main/java/com/springboot/javascript/JavascriptApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.javascript;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavascriptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavascriptApplication.class, args);
+    }
+
+}
---
+++ javascript/src/test/java/com/springboot/javascript/JavascriptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.javascript;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavascriptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
